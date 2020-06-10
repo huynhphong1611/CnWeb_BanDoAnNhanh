836700e80b787caaded01214d9f2ce3a99689f2a
huynhphong1611 <huynhvanphong123456@gmail.com>
2020-06-11 00:25:40 +0700

    them mon an tuan anh

--- Website_BuyFood/Areas/Admin/Controllers/HomeController.cs
+++ Website_BuyFood/Areas/Admin/Controllers/HomeController.cs
@@ -22,5 +22,33 @@ namespace Website_BuyFood.Areas.Admin.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult add(MonAn data, HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string path = Server.MapPath("/Content/images/" + file.FileName);
+                file.SaveAs(path);
+                //
+                MonAn ins = new MonAn();
+                ins.MaMon = context.MonAns.ToList().Count() + 1;
+                ins.TenMon = data.TenMon;
+                ins.LinkAnh = file.FileName;
+                ins.DonGia = data.DonGia;
+                context.MonAns.Add(ins);
+                context.SaveChanges();
+            }
+            else
+            {
+                MonAn ins = new MonAn();
+                ins.MaMon = context.MonAns.ToList().Count() + 1;
+                ins.TenMon = data.TenMon;
+                ins.LinkAnh = "Không có ảnh";
+                ins.DonGia = data.DonGia;
+                context.MonAns.Add(ins);
+                context.SaveChanges();
+            }
+            return RedirectToAction("index", "home");
+        }
     }
 }
